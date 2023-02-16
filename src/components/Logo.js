4c6ed87dd9319d0ae85166fadb110ef1295aee2c
jonathanm98 <jonathanmenelet@hotmail.fr>
2023-02-16 01:13:38 +0100

    added logo component

---
+++ src/components/Logo.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div>
+      <div className="main">
+        <header>
+          <img width="100px" src="./logo.png" alt="" />
+        </header>
+      </div>
+    </div>
+  );
+};
+
+export default Logo;
