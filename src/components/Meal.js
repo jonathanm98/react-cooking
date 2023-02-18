1618731621a96fe121e65e6db00273f5426693e0
jonathanm98 <jonathanmenelet@hotmail.fr>
2023-02-18 17:59:39 +0100

    card infos

--- src/components/Meal.js
+++ src/components/Meal.js
@@ -1,9 +1,14 @@
 import React from "react";
 
 const Meal = ({ recipe }) => {
+  console.log(recipe);
   return (
     <div className="meal-card">
-      <h3>{recipe.strMeal}</h3>
+      <img src={recipe.strMealThumb} alt={recipe.strMeal} />
+      <div className="meal-card-infos">
+        <h2>{recipe.strMeal}</h2>
+        <h3>{recipe.strCategory}</h3>
+      </div>
     </div>
   );
 };
